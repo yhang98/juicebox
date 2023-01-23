508a48f211a32c1357a06eca83154de4cc4039c4
Hang Yang <yhang98@gmail.com>
2023-01-22 17:51:52 -0800

    final commit

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const { Client } = require("pg");
 const client = new Client({
-  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+    connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
 
 
--- index.js
+++ index.js
@@ -1,41 +1,35 @@
+const express = require('express');
+const { client } = require('./db');
+client.connect();
+
 require('dotenv').config();
 
-const PORT = 3000;
-const express = require('express');
 const server = express();
+
 
 const morgan = require('morgan');
 server.use(morgan('dev'));
 
+const bodyParser = require ('body-parser')
+
+server.use(bodyParser.json())
+
 server.use(express.json())
-
-const { client } = require('./db');
-client.connect();
-
-server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
-});
 
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
-
-server.get('/background/:color', (req, res, next) => {
-  res.send(`
-    <body style="background: ${ req.params.color };">
-      <h1>Hello World</h1>
-    </body>
-  `);
-});
-
-server.get('/add/:first/to/:second', (req, res, next) => {
-  res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
-   Number(req.params.first) + Number(req.params.second)
-  }</h1>`);
 
 server.use((req, res, next) => {
   console.log("<____Body Logger START____>");
   console.log(req.body);
   console.log("<_____Body Logger END_____>");
-  
+
   next();
+});
+
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  // old stuff
+  console.log('The server is up on port', PORT)
 });
